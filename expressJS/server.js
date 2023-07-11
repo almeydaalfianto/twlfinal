f34eb8b95f611b4f63bc383a5c36f80249ae43ad
almeydaalfianto <almeyda.convergence.al@gmail.com>
2023-07-11 18:59:56 +0700

    final twl

---
+++ expressJS/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const pendudukroute = require("./router/pendudukroute");
+const userroute = require("./router/userroute");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3001;
+
+// Replace <password> with your actual password
+const uri = "mongodb+srv://almeyda:almeyda@cluster0.pimyglx.mongodb.net/?retryWrites=true&w=majority";
+
+app.use("/data", pendudukroute);
+app.use("/user", userroute);
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Menu from "./component/menu";
+import Login from "./component/login";
+import Register from "./component/register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Menu</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
